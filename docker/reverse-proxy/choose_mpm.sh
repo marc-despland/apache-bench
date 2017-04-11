6fb5122160bbbc3a0ae35bb53839ee29bde7a220
Marc Despland <marc.despland@art122-5.net>
2017-04-11 17:06:17 +0200

    new apache conf

--- docker/reverse-proxy/choose_mpm.sh
+++ docker/reverse-proxy/choose_mpm.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
-
-if [ '$1' == 'prefork' or  '$1' == 'event' or '$1' == 'worker' ] 
-then
-	ln -s /etc/apache2/mod-available/mpm_$1.conf /etc/apache2/mods-enabled/mpm.conf
-	ln -s /etc/apache2/mod-available/mpm_$1.load /etc/apache2/mods-enabled/mpm.load
-fi
+case "$1" in
+	"prefork")
+		rm /etc/apache2/mods-enabled/mpm.conf
+		rm /etc/apache2/mods-enabled/mpm.load
+		ln -s /etc/apache2/mod-available/mpm_$1.conf /etc/apache2/mods-enabled/mpm.conf
+		ln -s /etc/apache2/mod-available/mpm_$1.load /etc/apache2/mods-enabled/mpm.load
+		;;
+	"event")
+		rm /etc/apache2/mods-enabled/mpm.conf
+		rm /etc/apache2/mods-enabled/mpm.load
+		ln -s /etc/apache2/mod-available/mpm_$1.conf /etc/apache2/mods-enabled/mpm.conf
+		ln -s /etc/apache2/mod-available/mpm_$1.load /etc/apache2/mods-enabled/mpm.load
+		;;
+	"worker")
+		rm /etc/apache2/mods-enabled/mpm.conf
+		rm /etc/apache2/mods-enabled/mpm.load
+		ln -s /etc/apache2/mod-available/mpm_$1.conf /etc/apache2/mods-enabled/mpm.conf
+		ln -s /etc/apache2/mod-available/mpm_$1.load /etc/apache2/mods-enabled/mpm.load
+		;;
+	*)
+		echo Usage : $0 [prefork|event|worker]
+		;;
+esac
